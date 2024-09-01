7e6a6bd8832428a42bd390f8d8ca8a5d457a0977
Hong An Tran <hongantran3804@gmail.com>
2024-09-01 14:55:40 -0700

    final

--- app/page.jsx
+++ app/page.jsx
@@ -16,6 +16,7 @@ const Home = () => {
   const [chosenCountry, setChosenCountry] = useState("United States");
   const [chosenState, setChosenState] = useState("CA");
   const [forecastWeather, setForecastWeather] = useState([]);
+  const [showDesc, setShowDesc] = useState(false);
   const now = dayjs();
 
   useEffect(() => {
@@ -34,11 +35,11 @@ const Home = () => {
   }, []);
   useEffect(() => {
     const getForecastWeather = async () => {
-      const Weather_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${data.lat}&lon=${data.lon}&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_API}`;
-     
+      const Weather_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.lat}&lon=${data.lon}&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_API}`;
+
       try {
         const res = await fetch(Weather_API_URL);
-        
+
         if (res.ok) {
           let checkDays = [];
           const fiveDays = await res.json();
@@ -61,9 +62,7 @@ const Home = () => {
               iconCode: dayInfo.weather[0].icon,
             }));
           setForecastWeather(() => newDays);
-          
         }
-       
       } catch (e) {
         alert(e, data.lat, data.lon);
       }
@@ -71,7 +70,6 @@ const Home = () => {
     if (data.lat && data.lon) {
       getForecastWeather();
     }
-    
   }, [data]);
   const getWeatherData = async (city) => {
     if (city === "") {
@@ -109,152 +107,181 @@ const Home = () => {
       } catch (e) {
         alert("Something went wrong");
       }
-    
-    } 
+    }
   };
   return (
-    <section className="flex flex-col items-center justify-center w-full h-full overflow-hidden">
-      <div className="flex flex-row items-center gap-2">
+    <section className="overflow-x-hidden">
+      <div className="flex flex-col items-center justify-center w-full h-full gap-3">
+        <h1 className="text-[2rem]">Hong An Weather App</h1>
         <div
-          className="w-fit p-10 bg-gradient-to-b from-indigo-500 
-      to-indigo-900 flex flex-col items-center rounded-[10px] gap-[2rem]"
+          className="border-[1px] p-1 border-black bg-gray-100 hover:bg-gray-200 cursor-pointer active:bg-gray-300"
+          onClick={(e) => {
+            setShowDesc(!showDesc);
+          }}
         >
-          <div className="flex flex-row justify-between w-full gap-2 relative">
-            <input
-              placeholder="Search"
-              type="text"
-              className="p-2 rounded-[20px] pl-3 "
-              ref={city}
-              defaultValue="Irvine"
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  getWeatherData(city.current.value);
-                }
-              }}
-            />
-            {chosenCountry === "United States" && (
+          PM Accelerator description
+        </div>
+        {showDesc && (
+          <p className="text-center w-[60%]">
+            <p>
+              The Product Manager Accelerator Program is designed to support PM
+              professionals through every stage of their career. From students
+              looking for entry-level jobs to Directors looking to take on a
+              leadership role, our program has helped over hundreds of students
+              fulfill their career aspirations.
+            </p>
+            <p>
+              Our Product Manager Accelerator community are ambitious and
+              committed. Through our program they have learnt, honed and
+              developed new PM and leadership skills, giving them a strong
+              foundation for their future endeavours.
+            </p>
+          </p>
+        )}
+        <div className="flex flex-row items-center gap-2">
+          <div
+            className="w-fit p-10 bg-gradient-to-b from-indigo-500 
+      to-indigo-900 flex flex-col items-center rounded-[10px] gap-[2rem]"
+          >
+            <div className="flex flex-row justify-between w-full gap-2 relative">
+              <input
+                placeholder="Search"
+                type="text"
+                className="p-2 rounded-[20px] pl-3 "
+                ref={city}
+                defaultValue="Irvine"
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    getWeatherData(city.current.value);
+                  }
+                }}
+              />
+              {chosenCountry === "United States" && (
+                <select
+                  className="flex flex-col w-fit relative"
+                  onChange={(e) => {
+                    setChosenState(e.target.value);
+                  }}
+                >
+                  <option value="" selected>
+                    California
+                  </option>
+                  {stateList &&
+                    stateList.map((state) => (
+                      <option
+                        value={state.abbreviation}
+                        key={state.abbreviation}
+                      >
+                        {state.name}
+                      </option>
+                    ))}
+                </select>
+              )}
               <select
-                className="flex flex-col w-fit relative"
+                className="flex flex-col w-[10rem] relative "
                 onChange={(e) => {
-                  setChosenState(e.target.value);
+                  setChosenCountry(e.target.value);
                 }}
               >
                 <option value="" selected>
-                  California
+                  United States
                 </option>
-                {stateList &&
-                  stateList.map((state) => (
-                    <option value={state.abbreviation} key={state.abbreviation}>
-                      {state.name}
+                {countries &&
+                  countries.map((country) => (
+                    <option value={country.name} key={country.code}>
+                      {country.name}
                     </option>
                   ))}
               </select>
-            )}
-            <select
-              className="flex flex-col w-[10rem] relative "
-              onChange={(e) => {
-                setChosenCountry(e.target.value);
-              }}
-            >
-              <option value="" selected>
-                United States
-              </option>
-              {countries &&
-                countries.map((country) => (
-                  <option value={country.name} key={country.code}>
-                    {country.name}
-                  </option>
-                ))}
-            </select>
 
-            <div
-              className="bg-white p-3 rounded-full flex items-center justify-center cursor-pointer"
-              onClick={(e) => {
-                getWeatherData(city.current.value);
-              }}
-            >
-              <Image src={searchIcon} className="w-[0.9rem]" />
+              <div
+                className="bg-white p-3 rounded-full flex items-center justify-center cursor-pointer"
+                onClick={(e) => {
+                  getWeatherData(city.current.value);
+                }}
+              >
+                <Image src={searchIcon} className="w-[0.9rem]" />
+              </div>
             </div>
+            {weatherOk && (
+              <div className="flex flex-col items-center w-full">
+                <div className="flex flex-col items-center text-white">
+                  <div>
+                    <Image
+                      src={`https://openweathermap.org/img/wn/${data.iconCode}@2x.png`}
+                      width={150}
+                      height={150}
+                    />
+                  </div>
+                  <div className="flex flex-col font-bold items-center text-center">
+                    <div className="flex flex-col items-center text-center w-full">
+                      <span className="text-[3rem] m-0 p-0">{data.temp}°C</span>
+                      <span className="text-[1rem] m-0 p-0">{data.desc}</span>
+                      <p className="flex flex-row items-center justify-between w-full ">
+                        <span className="text-[1rem] m-0 p-0">
+                          H: {data.maxTemp}°C
+                        </span>
+                        <span className="text-[1rem] m-0 p-0">
+                          L: {data.minTemp}°C
+                        </span>
+                      </p>
+                    </div>
+                    <span className="text-[1.5rem]">My Location</span>
+                    <span className="text-[1rem] m-0 p-0">{data.location}</span>
+                  </div>
+                </div>
+                <div className="flex flex-row justify-between text-white w-full mt-[3rem]">
+                  <div className="flex flex-row">
+                    <div>
+                      <Image
+                        src={humidityIcon}
+                        className="w-[1rem] h-[1rem] m-1 mt-2"
+                      />
+                    </div>
+                    <div className="p-0 ">
+                      <p>{data.humidity}%</p>
+                      <p>Humidity</p>
+                    </div>
+                  </div>
+                  <div className="flex flex-row">
+                    <div>
+                      <Image
+                        src={windIcon}
+                        className="w-[1rem] h-[1rem] m-1 mt-2"
+                      />
+                    </div>
+                    <div className="p-0">
+                      <p>{data.windSpeed} Km/h</p>
+                      <p>Wind Speed</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
-          {weatherOk && (
-            <div className="flex flex-col items-center w-full">
-              <div className="flex flex-col items-center text-white">
-                <div>
-                  <Image
-                    src={`https://openweathermap.org/img/wn/${data.iconCode}@2x.png`}
-                    width={150}
-                    height={150}
-                  />
-                </div>
-                <div className="flex flex-col font-bold items-center text-center">
-                  <div className="flex flex-col items-center text-center w-full">
-                    <span className="text-[3rem] m-0 p-0">{data.temp}°C</span>
-                    <span className="text-[1rem] m-0 p-0">{data.desc}</span>
-                    <p className="flex flex-row items-center justify-between w-full ">
-                      <span className="text-[1rem] m-0 p-0">
-                        H: {data.maxTemp}°C
-                      </span>
-                      <span className="text-[1rem] m-0 p-0">
-                        L: {data.minTemp}°C
-                      </span>
-                    </p>
-                  </div>
-                  <span className="text-[1.5rem]">My Location</span>
-                  <span className="text-[1rem] m-0 p-0">{data.location}</span>
-                </div>
-              </div>
-              <div className="flex flex-row justify-between text-white w-full mt-[3rem]">
-                <div className="flex flex-row">
-                  <div>
+          {forecastWeather.length !== 0 && weatherOk && (
+            <div className="h-full flex flex-col items-start w-[40rem]">
+              <ul className="flex-1 p-5 grid grid-cols-3 w-full gap-3">
+                {forecastWeather.map((eachDay) => (
+                  <li
+                    className="text-white bg-gradient-to-b from-indigo-500 to-indigo-900 p-5 rounded-[10px] gap-2"
+                    key={eachDay.date}
+                  >
+                    <h2>{eachDay.date}</h2>
                     <Image
-                      src={humidityIcon}
-                      className="w-[1rem] h-[1rem] m-1 mt-2"
+                      src={`https://openweathermap.org/img/wn/${eachDay.iconCode}@2x.png`}
+                      width={150}
+                      height={150}
                     />
-                  </div>
-                  <div className="p-0 ">
-                    <p>{data.humidity}%</p>
-                    <p>Humidity</p>
-                  </div>
-                </div>
-                <div className="flex flex-row">
-                  <div>
-                    <Image
-                      src={windIcon}
-                      className="w-[1rem] h-[1rem] m-1 mt-2"
-                    />
-                  </div>
-                  <div className="p-0">
-                    <p>{data.windSpeed} Km/h</p>
-                    <p>Wind Speed</p>
-                  </div>
-                </div>
-              </div>
+                    <h3>Temp: {eachDay.temp}°C</h3>
+                    <h3>Wind: {eachDay.windSpeed}Km/h</h3>
+                    <h3>Humidity: {eachDay.humidity}%</h3>
+                  </li>
+                ))}
+              </ul>
             </div>
           )}
         </div>
-        {forecastWeather.length !== 0 && weatherOk && (
-          <div className="h-full flex flex-col items-start w-[40rem]">
-            <h1 className="text-[2rem] p-5">5-Day Forecast</h1>
-            <ul className="flex-1 p-5 grid grid-cols-3 w-full gap-3">
-              {forecastWeather.map((eachDay) => (
-                <li
-                  className="text-white bg-gradient-to-b from-indigo-500 to-indigo-900 p-5 rounded-[10px] gap-2"
-                  key={eachDay.date}
-                >
-                  <h2>{eachDay.date}</h2>
-                  <Image
-                    src={`https://openweathermap.org/img/wn/${eachDay.iconCode}@2x.png`}
-                    width={150}
-                    height={150}
-                  />
-                  <h3>Temp: {eachDay.temp}°C</h3>
-                  <h3>Wind: {eachDay.windSpeed}Km/h</h3>
-                  <h3>Humidity: {eachDay.humidity}%</h3>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
       </div>
     </section>
   );
